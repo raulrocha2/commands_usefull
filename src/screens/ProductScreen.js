6fa3e426f3f5cfcd29a4bd699f8558e4781a6702
raulrocha2 <raul.rocha2@gmail.com>
2021-02-10 08:54:52 -0300

    Commit React

---
+++ src/screens/ProductScreen.js
@@ -0,0 +1,86 @@
+import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { Row, Col, Image, ListGroup, Button, Card } from 'react-bootstrap'
+//Rating (Avaliação of User count star 
+import Rating from '../components/Rating'
+import Loader from '../components/Loader'
+import Message from '../components/Message'
+import { detailsProducts } from '../actions/productActions'
+
+
+function ProductScreen({ match }) {
+    
+    const dispatch = useDispatch()
+    const productDetails = useSelector(state => state.productDetails)
+    const { error, loading, product } = productDetails
+    
+    useEffect(() => {
+        dispatch(detailsProducts(match.params.id))
+    
+    }, [dispatch, match])
+    
+    
+    return (
+        <div>
+            <Link to='/' className='btn btn-light my-3'>Go Back</Link>
+    
+            <Row>
+                <Col md={6}>
+                    <Image src={product.image} alt={product.name} fluid></Image>
+                </Col>
+                <Col md={3}>
+                    <ListGroup variant="flush">
+                        <ListGroup.Item>
+                            <h3>{product.name}</h3>
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                            <Rating 
+                            value={product.rating} text={`${product.numReviews} ratings`} color={'#f8e825'}
+                            />
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                            Price: ${product.price}
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                            Descripetion: {product.description}
+                        </ListGroup.Item>
+                    </ListGroup>
+                </Col>
+                <Col md={3}>
+                    <Card>
+                        <ListGroup>
+                            <ListGroup variant = "flush">
+                                <ListGroup.Item>
+                                    <Row>
+                                        <Col>Price:</Col>
+                                        <Col>
+                                            <strong>${product.price}</strong>
+                                        </Col>
+                                    </Row>
+                                </ListGroup.Item>
+                                <ListGroup.Item>
+                                    <Row>
+                                        <Col>Status:</Col>
+                                        <Col>
+                                            {product.countInStock > 0 ? 'In Stock' : 'Out If Stock'}
+                                        </Col>
+                                    </Row>
+                                </ListGroup.Item>
+                                <ListGroup.Item>
+                                    <Button className='btn-block' type='button' disabled={product.countInStock == 0} type='button'>
+                                        Add to Cart
+                                    </Button>
+                                </ListGroup.Item>
+                            </ListGroup>
+                        </ListGroup>
+                    </Card>
+                </Col>
+            </Row>
+           
+            
+        </div>
+    )
+}
+
+export default ProductScreen
